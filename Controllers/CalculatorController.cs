7c5d01c7a468037c987a62da453d75bbbb84289c
Andrew Dutton <814207579@wwcc.edu>
2019-10-22 13:50:15 +0900

    Finished Homework

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using calculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calculatorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            CalculatorNumbers myNumbers = new CalculatorNumbers();
+            return View(myNumbers);
+        }
+
+        public IActionResult Add(float Number1, float Number2)
+        {
+            float calculatedNumber = Number1 + Number2;
+            return View(calculatedNumber);
+        }
+
+        public IActionResult Subtract(float Number1, float Number2)
+        {
+            float calculatedNumber = Number1 - Number2;
+            return View(calculatedNumber);
+        }
+
+        public IActionResult Divide(float Number1, float Number2)
+        {
+            float calculatedNumber = Number1 / Number2;
+            //just a check if they try to break it by dividing by 0 or not putting anything in
+            if (calculatedNumber.ToString() == "NaN" || calculatedNumber.ToString() == "∞")
+            {
+                calculatedNumber = 0;
+            }
+            return View(calculatedNumber);
+        }
+
+        public IActionResult Multiply(float Number1, float Number2)
+        {
+            float calculatedNumber = Number1 * Number2;
+            return View(calculatedNumber);
+        }
+
+
+
+    }
+}
---
+++ Models/CalculatorNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace calculatorApp.Models
+{
+    public class CalculatorNumbers
+    {
+        public float Number1 { get; set; }
+        public float Number2 { get; set; }
+    }
+}
